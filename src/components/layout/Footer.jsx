5ecc57169bf8c638351e165396c30e3c71d4b771
krzysztof-siek <krzysztof.siek01@gmail.com>
2020-11-04 21:29:43 +0100

    initial commit

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+	return ( 
+		<footer>
+			<div className="footer">
+				<h1>footer</h1>
+			</div>
+		</footer>
+	 );
+}
+ 
+export default Footer;
