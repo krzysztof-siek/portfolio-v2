8606be83dc7af6b336a32cd5297940849aa9bb09
krzysztof-siek <krzysztof.siek01@gmail.com>
2020-11-05 21:14:08 +0100

    create basic structure of nav

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
 
-
 const Header = () => {
-	return ( 
-		<header>
-			<div className="header">
-				<h1>header</h1>
-			</div>
-		</header>
-	 );
+    return (
+        <header>
+            <div className="container">
+                <div className="header">
+                    <div className="nav-brand">
+                        <picture>
+                            <img src='/img/logo.png' alt="logo"/>
+                        </picture>
+                    </div>
+                    <ul className="nav-links">
+                        <li className="nav-item">
+                            <a href="#" className="nav-link active">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link">Skills</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link">Projects</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+    );
 }
- 
+
 export default Header;
